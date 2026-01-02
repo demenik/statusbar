95908472dee916268a03eab98a74ba64b2d04af2
demenik <mail@demenik.dev>
2026-01-02 18:39:18 +0100

    fix(marquee): Fix marquee (fade not working rn)

--- widgets/marquee.tsx
+++ widgets/marquee.tsx
@@ -1,9 +1,9 @@
 import { Gtk } from "ags/gtk4";
-import { Accessor, createState } from "ags";
-import { timeout, type Timer } from "ags/time";
-import Pango from "gi://Pango";
-import { isAccessor } from "../utils/accessor";
+import { Accessor, createEffect, createMemo, createState } from "gnim";
 import { Props } from "../utils/types";
+import { isAccessor } from "../utils/accessor";
+import Pango from "gi://Pango?version=1.0";
+import { timeout, Timer } from "ags/time";
 import { FadeBox } from "./fade";
 
 type MarqueeProps = {
@@ -25,95 +25,136 @@ export const MarqueeLabel = ({
   labelProps = {},
   ...rest
 }: MarqueeProps) => {
-  let marqueeTimer: Timer | null = null;
+  console.log("INIT MarqueeLabel");
+
+  let timer: Timer | null = null;
   let fadeTimer: Timer | null = null;
+  let labelWidget: Gtk.Label | null = null;
 
-  const [boxCss, setBoxCss] = createState(
-    "transition: none; transform: translateX(0);",
-  );
-  const [fadeSides, setFadeSides] = createState([Gtk.PositionType.RIGHT]);
-
-  const [width, setWidth] = createState(maxWidth ?? -1);
-  const [height, setHeight] = createState(-1);
-
-  const updateMarquee = (container: Gtk.Overlay, label: Gtk.Label) => {
-    if (marqueeTimer) {
-      marqueeTimer.cancel();
-      marqueeTimer = null;
-    }
-    if (fadeTimer) {
-      fadeTimer.cancel();
-      fadeTimer = null;
-    }
-
-    setBoxCss("transition: none; transform: translateX(0);");
-    setWidth(maxWidth ?? -1);
-
-    const pangoCtx = label.get_pango_context();
-    const layout = Pango.Layout.new(pangoCtx);
-    layout.set_text(isAccessor(text) ? text() : text, -1);
-    const [labelWidth, labelHeight] = layout.get_pixel_size();
-    const containerWidth = container.get_preferred_size()[0]?.width ?? -1;
-
-    setHeight(labelHeight);
-
-    if (labelWidth > containerWidth) {
-      const scrollAmount = labelWidth + marqueeGap;
-      const duration = (scrollAmount / marqueeSpeed) * 1000;
-
-      setFadeSides([Gtk.PositionType.RIGHT]);
-
-      const animate = () => {
-        setBoxCss(`
-          transition: transform ${duration}ms linear;
-          transform: translateX(-${scrollAmount}px);
-        `);
-        setFadeSides([Gtk.PositionType.LEFT, Gtk.PositionType.RIGHT]);
-
-        fadeTimer = timeout(duration * 0.95, () =>
-          setFadeSides([Gtk.PositionType.RIGHT]),
-        );
-
-        marqueeTimer = timeout(duration, () => {
-          setBoxCss(`
-            transition: none;
-            transform: translateX(0);
-          `);
-
-          marqueeTimer = timeout(marqueePause, animate);
-        });
-      };
-
-      marqueeTimer = timeout(marqueePause, animate);
-    } else {
-      setWidth(labelWidth);
-      setFadeSides([]);
-    }
+  const cleanup = () => {
+    timer?.cancel();
+    timer = null;
+    fadeTimer?.cancel();
+    fadeTimer = null;
   };
 
+  const value = isAccessor(text) ? text : () => text as string;
+
+  const [labelWidth, setLabelWidth] = createState(maxWidth ?? -1);
+  const [labelHeight, setLabelHeight] = createState(-1);
+  const updateLabelWidth = (label: Gtk.Label) => {
+    const pangoCtx = label.get_pango_context();
+    const layout = Pango.Layout.new(pangoCtx);
+    layout.set_text(value(), -1);
+
+    const [w, h] = layout.get_pixel_size();
+    if (w > 0) setLabelWidth(w);
+    if (h > 0) setLabelHeight(h);
+  };
+
+  const [containerWidth, setContainerWidth] = createState(maxWidth ?? -1);
+  const updateContainerWidth = (container: Gtk.Overlay) => {
+    const w = container.get_allocated_width();
+    if (w > 0) setContainerWidth(w);
+  };
+
+  const shouldScroll = createMemo(() => labelWidth() > containerWidth());
+  const width = createMemo(() =>
+    shouldScroll() ? containerWidth() : labelWidth(),
+  );
+
+  const [isScrolling, setIsScrolling] = createState(false);
+  const [isNearEnd, setIsNearEnd] = createState(true);
+
+  const fadeSides = createMemo(() => {
+    let sides: Gtk.PositionType[] = [];
+    if (shouldScroll()) sides.push(Gtk.PositionType.RIGHT);
+    if (isScrolling() && !isNearEnd()) sides.push(Gtk.PositionType.LEFT);
+    return sides;
+  });
+
+  const scrollAmount = labelWidth.as((value) => value + marqueeGap);
+  const duration = scrollAmount.as((value) =>
+    Math.round((value / marqueeSpeed) * 1000),
+  );
+  const boxCss = createMemo(() =>
+    isScrolling()
+      ? `transition: transform ${duration()}ms linear; transform: translateX(-${scrollAmount()}px);`
+      : "transition: none; transform: translateX(0);",
+  );
+
+  const startMarquee = () => {
+    cleanup();
+    console.log(`[MarqueeLabel] ${value()} is waiting`);
+    timer = timeout(marqueePause, () => {
+      console.log(`[MarqueeLabel] ${value()} is scrolling`);
+      setIsNearEnd(false);
+      setIsScrolling(true);
+
+      timer = timeout(duration(), () => {
+        setIsScrolling(false);
+        startMarquee();
+      });
+
+      fadeTimer = timeout(duration() * 0.95, () => {
+        setIsNearEnd(true);
+      });
+    });
+  };
+
+  createEffect(() => {
+    value();
+    if (labelWidget) updateLabelWidth(labelWidget);
+  });
+
+  createEffect(() => {
+    if (shouldScroll()) {
+      startMarquee();
+    } else {
+      cleanup();
+      setIsScrolling(false);
+      setIsNearEnd(true);
+    }
+  });
+
+  createEffect(() => {
+    console.log(
+      `[MarqueeLabel] ${JSON.stringify(
+        {
+          value: value(),
+          labelWidth: labelWidth(),
+          labelHeight: labelHeight(),
+          containerWidth: containerWidth(),
+          shouldScroll: shouldScroll(),
+          scrollAmount: scrollAmount(),
+          duration: duration(),
+        },
+        null,
+        2,
+      )}`,
+    );
+  });
+
   return (
-    <FadeBox fadeSides={fadeSides} fadeWidth={20} {...rest}>
+    <FadeBox fadeSides={[]} fadeWidth={20} {...rest}>
       <overlay
         class="marquee-container"
         css="min-width: 0;"
         overflow={Gtk.Overflow.HIDDEN}
         halign={Gtk.Align.BASELINE_CENTER}
-        $={(self) => {
-          const box = self.get_last_child()! as Gtk.Box;
-          const label = box.get_first_child()! as Gtk.Label;
-
-          if (isAccessor(text)) {
-            text.subscribe(() => {
-              timeout(1, () => updateMarquee(self, label));
-            });
-          }
-
-          self.connect("realize", () => updateMarquee(self, label));
-        }}
+        onMap={(self) => updateContainerWidth(self)}
+        onDestroy={() => cleanup()}
       >
-        <box widthRequest={width} heightRequest={height} vexpand={true} />
+        <box widthRequest={width} heightRequest={labelHeight} vexpand={true} />
         <box $type="overlay" css={boxCss}>
-          <label {...labelProps} label={text} />
+          <label
+            {...labelProps}
+            label={text}
+            onMap={(self) => {
+              labelWidget = self;
+              updateLabelWidth(self);
+            }}
+          />
           <box widthRequest={marqueeGap} />
           <label {...labelProps} label={text} />
         </box>
